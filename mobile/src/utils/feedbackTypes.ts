5fef0041dc58ce221398ed496be77ef9df3f2944
LeonardoMonti <LeonardoMonti>
2022-05-06 14:30:45 -0300

    mobile

---
+++ mobile/src/utils/feedbackTypes.ts
@@ -0,0 +1,18 @@
+export const feedbackTypes = {
+  'BUG': {
+    title: 'Problema',
+    placeHolder: 'Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo...',
+    image: require('../assets/bug.png')
+  },
+  'IDEA': {
+    title: 'Ideia',
+    placeHolder: 'Teve uma ideia de melhoria ou de nova funcionalidade? Conta pra gente!',
+    image: require('../assets/idea.png'),
+  },
+  'OTHER': {
+    title: 'Outro',
+    placeHolder: 'Queremos te ouvir. O que você gostaria de nos dizer?',
+    image: require('../assets/thought.png'),
+  },
+};
+
